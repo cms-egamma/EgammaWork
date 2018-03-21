42ee43971082d4dc48131e21b1a2f9fd342903b3
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-03-21 12:07:21 +0100

    Actually the same as runElectrons.py, but with AOD flag set

---
+++ ElectronNtupler/test/runElectrons_AOD.py
@@ -0,0 +1,86 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Ntupler")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+# NOTE: the pick the right global tag!
+#    for Spring15 25ns MC: global tag is 'auto:run2_mc'
+#    for Run 2 data: global tag is 'auto:run2_data'
+#  as a rule, find the "auto" global tag in $CMSSW_RELEASE_BASE/src/Configuration/AlCa/python/autoCond.py
+#  This auto global tag will look up the "proper" global tag
+#  that is typically found in the DAS under the Configs for given dataset
+#  (although it can be "overridden" by requirements of a given release)
+from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2017_realistic', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '94X_mc2017_realistic_v12', '')
+
+#
+# Define input data to read
+#
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+
+inputFilesAOD = cms.untracked.vstring(
+    # AOD test files from /DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10_ext1-v1/AODSIM
+    # ... files are missing ...
+    )    
+
+inputFilesMiniAOD = cms.untracked.vstring(
+    # MiniAOD test files from /DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10_ext1-v1/MINIAODSIM
+    '/store/mc/RunIIFall17MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/00000/005DC030-D3F4-E711-889A-02163E01A62D.root',
+    )
+
+#
+# You can list here either AOD or miniAOD files, but not both types mixed
+#
+useAOD = True
+if useAOD == True :
+    inputFiles = inputFilesAOD
+    outputFile = "electron_ntuple.root"
+    pileupProductName = "addPileupInfo"
+    print("AOD input files are used")
+else :
+    inputFiles = inputFilesMiniAOD
+    outputFile = "electron_ntuple_mini.root"
+    pileupProductName = "slimmedAddPileupInfo"
+    print("MiniAOD input files are used")
+process.source = cms.Source ("PoolSource", fileNames = inputFiles )                             
+
+#
+# Configure the ntupler module
+#
+
+process.ntupler = cms.EDAnalyzer('SimpleElectronNtupler',
+                                 # The module automatically detects AOD vs miniAOD, so we configure both
+                                 #
+                                 # Common to all formats objects
+                                 #
+                                 pileup   = cms.InputTag( pileupProductName ),
+                                 rho      = cms.InputTag("fixedGridRhoFastjetAll"),
+                                 beamSpot = cms.InputTag('offlineBeamSpot'),
+                                 genEventInfoProduct = cms.InputTag('generator'),
+                                 #
+                                 # Objects specific to AOD format
+                                 #
+                                 electrons    = cms.InputTag("gedGsfElectrons"),
+                                 genParticles = cms.InputTag("genParticles"),
+                                 vertices     = cms.InputTag("offlinePrimaryVertices"),
+                                 conversions  = cms.InputTag('allConversions'),
+                                 #
+                                 # Objects specific to MiniAOD format
+                                #
+                                 electronsMiniAOD    = cms.InputTag("slimmedElectrons"),
+                                 genParticlesMiniAOD = cms.InputTag("prunedGenParticles"),
+                                 verticesMiniAOD     = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                                 conversionsMiniAOD  = cms.InputTag('reducedEgamma:reducedConversions'),
+                                 # Effective areas for computing PU correction for isolations
+                                 effAreasConfigFile = cms.FileInPath("EgammaWork/ElectronNtupler/data/effAreaElectrons_cone03_pfNeuHadronsAndPhotons_94X.txt")
+                                 )
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string( outputFile )
+                                   )
+
+
+process.p = cms.Path(process.ntupler)
